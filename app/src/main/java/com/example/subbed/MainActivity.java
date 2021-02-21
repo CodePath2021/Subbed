e3932ad68c480da88b6938f5c2a18c018ca8b735
DameDollaForThree <j1cai@ucsd.edu>
2021-02-20 17:42:11 -0800

    first commit

---
+++ app/src/main/java/com/example/subbed/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.subbed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
